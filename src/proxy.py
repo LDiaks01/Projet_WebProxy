a7db5cac17d3c40f0a9d3bde6ae62f249db2e3da
Lansana Diakite <diakslead@gmail.com>
2024-01-01 01:57:58 +0100

    the http version is working now

--- src/proxy.py
+++ src/proxy.py
@@ -14,49 +14,74 @@ class ProxyServer:
     def start(self):
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((self.host, self.port))
-        self.server_socket.listen(5)
+        self.server_socket.listen(socket.SOMAXCONN)
         print(f"Proxy Server listening on {self.host}:{self.port}")
 
         while True:
             client_socket, client_address = self.server_socket.accept()
-            print(client_socket)
+            
             threading.Thread(target=self.handle_client, args=(client_socket,)).start()
 
     def handle_client(self, client_socket):
-        request_data = client_socket.recv(4096)
+        request_data = b""
         
+        while True:
+            # Recevoir les données par tranches de 4096 octets
+            chunk = client_socket.recv(4096)
+            if not chunk:
+                # La connexion a été fermée par le client
+                break
+            request_data += chunk
 
-        # Modifier la requête
+            # Si le dernier morceau de données reçu est moins de 4096 octets, cela signifie que toutes les données ont été reçues
+            if len(chunk) < 4096:
+                break
+
+        # Vérifier si la requête commence par le verbe "CONNECT"
+        is_https_request = request_data.startswith(b'CONNECT')
+
+        # Modifier la requête si nécessaire
         modified_request = self.modify_request(request_data)
-        print("Modifiied :", modified_request)
 
         # Extraire l'URL demandée à partir de la requête modifiée
         requested_host = self.extract_requested_host(modified_request)
-        print("Requested Host : ",requested_host)
 
         remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+        
         try:
-            # Connexion sécurisée avec le serveur distant sur le port 443
-            context = ssl.create_default_context()
-            remote_socket.connect((requested_host, 443))
-            remote_socket = context.wrap_socket(remote_socket, server_hostname=requested_host)
+            if is_https_request:
+                # Connexion sécurisée avec le serveur distant sur le port 443
+                context = ssl.create_default_context()
+                remote_socket.connect((requested_host, 443))
+                remote_socket = context.wrap_socket(remote_socket, server_hostname=requested_host)
+                remote_socket.sendall(modified_request)
+                
+            else:
+                # Connexion non sécurisée sur le port 80 pour les requêtes HTTP
+                remote_socket.connect((requested_host, 80))
+                remote_socket.sendall(modified_request)
 
-            remote_socket.sendall(modified_request)
-            response_data = remote_socket.recv(4096)
-            print("Response Data : ",response_data)
+            response_data = b""
+            while True:
+                # Recevoir les données par tranches de 4096 octets depuis le serveur distant
+                chunk = remote_socket.recv(4096)
+                if not chunk:
+                    # Toutes les données du serveur distant ont été reçues
+                    break
+                response_data += chunk
+
+            # print("Response Data : ", response_data)
         except Exception as e:
             print(e.args)
             # En cas d'échec, essayez la connexion non sécurisée sur le port 80
-            remote_socket.connect((requested_host, 80))
-            remote_socket.sendall(modified_request)
-            response_data = remote_socket.recv(4096)
+            # remote_socket.connect((requested_host, 80))
+            # remote_socket.sendall(modified_request)
+            # response_data = remote_socket.recv(4096)
 
         # Renvoyer la réponse au client
+        # print("Client Socket :", client_socket, " ---Reponse : ", response_data.decode()[:20])
         client_socket.sendall(response_data)
-        client_socket.close()
-        remote_socket.close()
-        
+
 
 
     def modify_request(self, request_data):
@@ -69,23 +94,28 @@ class ProxyServer:
         modified_request = modified_request.replace(b'HTTP/1.1', b'HTTP/1.0')
 
         return modified_request
+    
+    def extract_requested_host(self, request_data):
+      
+       
+        # Trouver le nom d'hôte à partir du champ "Host" dans l'en-tête
+        host_header = self.extract_host_header(request_data)
+        if host_header:
+            return host_header
 
-    def extract_requested_host(self, raw_request):
-        raw_request = raw_request.decode('utf-8')
-        lines = raw_request.split('\n')
-        
-        # Chercher l'en-tête Host
-        host_line = next((line for line in lines if line.lower().startswith('host:')), None)
-        
-        if host_line:
-            host = host_line.split(':')[1].strip()
-        elif 'HTTP/1.0' in lines[0]:
-            # Pour HTTP/1.0, le nom d'hôte doit être déduit de l'adresse IP
-            host = 'Nom d\'hôte pour HTTP/1.0 non spécifié dans la requête'
-        else:
-            host = 'En-tête Host non trouvé dans la requête'
+    def extract_host_header(self, request_data):
+        # Rechercher le champ "Host" dans l'en-tête
+        host_header = None
+        for line in request_data.split(b"\r\n"):
+            if line.lower().startswith(b"host:"):
+                # Vérifier si ":" est présent avant de diviser
+                if b":" not in line:
+                    host_header = line.split(b":", 1)[1].strip()
+                else:
+                    host_header = line.split(b":", 1)[1].strip().split(b":")[0]
+                break
+        return host_header
 
-        return host
 
 if __name__ == "__main__":
     proxy_instance = ProxyServer('127.0.0.1', 9000, 'test', 'test')
